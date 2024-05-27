06663c4f7fc09a39b730f74a1fdd112d2d285168
William Moore <w.moore@dundee.ac.uk>
2024-05-27 22:30:35 +0100

    vite build copies output to Django project

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    sourcemap: true,
+    // output into Django's static dir
+    outDir: "./omero_script_ui/static/omero_script_ui/"
+  },
 })
